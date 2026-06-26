436c2c9e0e6b3c40553407bf3fa71704bf82aa9f
bum8844 <crosschang3927@gmail.com>
2026-06-26 22:55:03 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+
+> 이 페이지를 [https://crosschang.github.io/literacy_class_makecode_extension/](https://crosschang.github.io/literacy_class_makecode_extension/)으로 열기
+
+## 이 확장은 전용확장입니다.
+
+이 저장소는 MakeCode에서 특정맵의 **확장**으로 사용중입니다.
+아직 배타버전입니다.
+
+#### 메타데이터(검색, 렌더링에 사용)
+
+* for PXT/minecraft
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ custom.ts
@@ -0,0 +1,574 @@
+/**
+ * Custom blocks
+ */
+
+//% weight=100 color=#7B61FF icon="\uf084" block="문열고 닫는 코드 최적화"
+namespace DoorControl {
+    let position: Position = null
+
+    //% blockId=OpenDoor block="$n째 문 열기"
+    export function OpenDoor(n: number){
+        position = player.position()
+        if (position.getValue(Axis.X) == 16 - n * 2 && (position.getValue(Axis.Y) == 6 && (position.getValue(Axis.Z) == -88 || position.getValue(Axis.Z) == -89))) {
+            if (n > 1 || n < 15) {
+                player.execute(
+                "/setblock" + " " + world(14 + -2 * (n - 1), 3, -87) + " " + "smooth_quartz"
+                )
+            } else {
+                player.execute(
+                "say 자기 위치에 맞는 이미지에 적힌 값으로만 적어주세요!"
+                )
+            }
+        } else {
+            player.execute(
+            "say 이미지에 적힌 값으로만 적어주세요!"
+            )
+        }
+    }
+
+    //% blockId=CloseDoor block="$n째 문 닫기"
+    export function CloseDoor(n: number){
+        position = player.position()
+        if (position.getValue(Axis.X) == 16 - n * 2 && (position.getValue(Axis.Y) == 6 && (position.getValue(Axis.Z) == -88 || position.getValue(Axis.Z) == -89))) {
+            if (n > 1 || n < 15) {
+                player.execute(
+                "/setblock" + " " + world(14 + -2 * (n - 1), 3, -87) + " " + "redstone_block"
+                )
+            } else {
+                player.execute(
+                "say 자기 위치에 맞는 이미지에 적힌 값으로만 적어주세요!"
+                )
+            }
+        } else {
+            player.execute(
+            "say 이미지에 적힌 값으로만 적어주세요!"
+            )
+        }
+    }
+}
+
+/**
+ * Custom blocks
+ */
+
+//% weight=100 color=#0fbc11 icon="\uf2bb" block="검사할문장리스트"
+namespace CheckList {
+
+    player.execute("function quiz/class2/testing")
+
+    let 검사문장모음: string[] = []
+
+    검사문장모음 = [
+        "나는 초등학생 3학년이야",
+        "나는 마인초등학교에 다니고 있어",
+        "오늘은 개인정보 보호를 배울 수 있어서 좋아",
+        "오늘 수업은 어렵지만 재미있어",
+        "코딩교육과 함께해서 신나",
+        "나의 전화번호는 010-9876-1234야",
+        "나의 어머니의 이름은 김마리야",
+        "나는 케이블아파트 209호에서 살아",
+        "나만의 채팅명령어를 쓸 수 있는 것은 신기해",
+        "내 이메일은 crafting@minecraft.com 이야",
+        "아버지의 번호는 010-2310-2313이야",
+        "내 집의 위치는 숲주민마을이고 주소는 숲주민마을길 137-5야",
+        "선생님이 오늘 알려주는 코딩은 나한테 도움이 되",
+        "문장들을 번호대로 나누니까 좋아보여"
+    ]
+    /**
+     * 검사할 문장 개수를 가져옵니다.
+     */
+    //% blockId=GetCheckList_len block="검사할 문장 개수"
+    export function GetCheckList_Length(): number {
+        return 검사문장모음.length;
+    }
+
+    /**
+     * 입력한 숫자에 맞는 검사문장을 가져옵니다.
+     * @param 번호 문장 번호, eg: 1
+     */
+    //% blockId=GetCheckList_sen block="$num 의 문장 가져오기"
+    export function GetCheckList_Sentence(num: number): string {
+        if (num < 1 || num > 검사문장모음.length) {
+            return ""
+        }
+        return 검사문장모음[num - 1]
+    }
+}
+
+let answer: number = 0, value: number = 0, blocktype = GOLD_BLOCK
+let player_number: number = 0, x: number = 0, z: number = 0
+
+interface PlayerData {
+    readonly tagname: string;
+    readonly player_number: number;
+    readonly x: number;
+    readonly z: number;
+}
+
+let players: PlayerData[] = [
+    {
+        tagname: "examination_1",
+        player_number: 1,
+        x: -26,
+        z: -9
+    },
+    {
+        tagname: "examination_2",
+        player_number: 2,
+        x: -26,
+        z: -5
+    },
+    {
+        tagname: "examination_3",
+        player_number: 3,
+        x: -26,
+        z: -1
+    },
+    {
+        tagname: "examination_4",
+        player_number: 4,
+        x: -26,
+        z: 3
+    },
+    {
+        tagname: "examination_5",
+        player_number: 5,
+        x: -26,
+        z: 7
+    },
+    {
+        tagname: "examination_6",
+        player_number: 6,
+        x: -26,
+        z: 11
+    },
+    {
+        tagname: "examination_7",
+        player_number: 7,
+        x: -26,
+        z: 15
+    },
+    {
+        tagname: "examination_8",
+        player_number: 8,
+        x: -34,
+        z: -9
+    },
+    {
+        tagname: "examination_9",
+        player_number: 9,
+        x: -34,
+        z: -5
+    },
+    {
+        tagname: "examination_10",
+        player_number: 10,
+        x: -34,
+        z: -1
+    },
+    {
+        tagname: "examination_11",
+        player_number: 11,
+        x: -34,
+        z: 3
+    },
+    {
+        tagname: "examination_12",
+        player_number: 12,
+        x: -34,
+        z: 7
+    },
+    {
+        tagname: "examination_13",
+        player_number: 13,
+        x: -34,
+        z: 11
+    },
+    {
+        tagname: "examination_14",
+        player_number: 14,
+        x: -34,
+        z: 15
+    },
+    {
+        tagname: "examination_15",
+        player_number: 15,
+        x: -30,
+        z: 19
+    },
+]
+
+
+function loadPlayerData(playerTable: PlayerData[]): boolean {
+    for (let k = 0; k < playerTable.length; k++) {
+        let data = playerTable[k]
+
+        if (player.execute("testfor @s[tag=" + data.tagname + "]")) {
+            player_number = data.player_number
+            x = data.x
+            z = data.z
+
+            return true
+        }
+    }
+
+    player.say("§c앗! 지정된 플레이어를 찾지못했습니다.")
+    return false
+}
+
+function classification_test(): void {
+    //player.say("정답 : " + answer)
+    //player.say("값 : " + value)
+    //player.say("플레이어 넘버 : " + player_number)
+    //player.say("x좌표 : " + x)
+    //player.say("z좌표 : " + z)
+    //player.say("블록 타입 : " + blocktype)
+    if (blocks.testForBlock(blocktype, world(x, 2, z))) {
+        if (value == answer) {
+            player.execute(
+                "dialogue " + "open " + "@e" + "[tag=computer" + player_number + ",tag=npc_day5] " + "@p" + "[tag=examination_" + player_number + "] " + "\"class5.answer.classification" + answer + "\""
+            )
+            player.execute(
+                "scoreboard " + "players " + "add " + "@p" + "[tag=examination_" + player_number + "] " + "score " + "1"
+            )
+            blocks.place(AIR, world(x, 2, z))
+        } else {
+            player.execute(
+                "tag " + "@p" + "[tag=examination_" + player_number + "] " + "add " + "classification"
+            )
+            player.execute(
+                "execute " + "@p" + "[tag=examination_" + player_number + "] " + "~ ~ ~ " + "function quiz/penalty"
+            )
+            blocks.place(AIR, world(x, 2, z))
+        }
+    }
+}
+
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#7B61FF block="정보 분류" icon="\uf002"
+namespace classification {
+
+    loadPlayerData(players)
+    player.execute("function quiz/class5/testing")
+
+    //% blockId=classification5_5 block="📢 : 광고/출처 모름"
+    export function data5_5(): void {
+        blocktype = DIAMOND_BLOCK
+        value = 5
+        answer = 5
+    }
+    //% blockId=classification5_4 block="💬 : 개인 의견"
+    export function data5_4(): void {
+        blocktype = DIAMOND_BLOCK
+        value = 4
+        answer = 5
+    }
+    //% blockId=classification5_3 block="📰 : 뉴스/책 자료"
+    export function data5_3(): void {
+        blocktype = DIAMOND_BLOCK
+        value = 3
+        answer = 5
+    }
+    //% blockId=classification5_2 block="👩‍🏫 : 전문가 자료"
+    export function data5_2(): void {
+        blocktype = DIAMOND_BLOCK
+        value = 2
+        answer = 5
+    }
+    //% blockId=classification5_1 block="🏛 : 공식 자료"
+    export function data5_1(): void {
+        blocktype = DIAMOND_BLOCK
+        value = 1
+        answer = 5
+    }
+    //% blockId=classification5 block="5번 문제"
+    export function Q5(): void {
+    }
+
+    //% blockId=classification4_5 block="📢 : 광고/출처 모름"
+    export function data4_5(): void {
+        blocktype = EMERALD_BLOCK
+        value = 5
+        answer = 4
+    }
+    //% blockId=classification4_4 block="💬 : 개인 의견"
+    export function data4_4(): void {
+        blocktype = EMERALD_BLOCK
+        value = 4
+        answer = 4
+    }
+    //% blockId=classification4_3 block="📰 : 뉴스/책 자료"
+    export function data4_3(): void {
+        blocktype = EMERALD_BLOCK
+        value = 3
+        answer = 4
+    }
+    //% blockId=classification4_2 block="👩‍🏫 : 전문가 자료"
+    export function data4_2(): void {
+        blocktype = EMERALD_BLOCK
+        value = 2
+        answer = 4
+    }
+    //% blockId=classification4_1 block="🏛 : 공식 자료"
+    export function data4_1(): void {
+        blocktype = EMERALD_BLOCK
+        value = 1
+        answer = 4
+    }
+    //% blockId=classification4 block="4번 문제"
+    export function Q4(): void {
+    }
+
+    //% blockId=classification3_5 block="📢 : 광고/출처 모름"
+    export function data3_5(): void {
+        blocktype = LAPIS_LAZULI_BLOCK
+        value = 5
+        answer = 3
+    }
+    //% blockId=classification3_4 block="💬 : 개인 의견"
+    export function data3_4(): void {
+        blocktype = LAPIS_LAZULI_BLOCK
+        value = 4
+        answer = 3
+    }
+    //% blockId=classification3_3 block="📰 : 뉴스/책 자료"
+    export function data3_3(): void {
+        blocktype = LAPIS_LAZULI_BLOCK
+        value = 3
+        answer = 3
+    }
+    //% blockId=classification3_2 block="👩‍🏫 : 전문가 자료"
+    export function data3_2(): void {
+        blocktype = LAPIS_LAZULI_BLOCK
+        value = 2
+        answer = 3
+    }
+    //% blockId=classification3_1 block="🏛 : 공식 자료"
+    export function data3_1(): void {
+        blocktype = LAPIS_LAZULI_BLOCK
+        value = 1
+        answer = 3
+    }
+    //% blockId=classification3 block="3번 문제"
+    export function Q3(): void {
+    }
+
+    //% blockId=classification2_5 block="📢 : 광고/출처 모름"
+    export function data2_5(): void {
+        blocktype = IRON_BLOCK
+        value = 5
+        answer = 2
+    }
+    //% blockId=classification2_4 block="💬 : 개인 의견"
+    export function data2_4(): void {
+        blocktype = IRON_BLOCK
+        value = 4
+        answer = 2
+    }
+    //% blockId=classification2_3 block="📰 : 뉴스/책 자료"
+    export function data2_3(): void {
+        blocktype = IRON_BLOCK
+        value = 3
+        answer = 2
+    }
+    //% blockId=classification2_2 block="👩‍🏫 : 전문가 자료"
+    export function data2_2(): void {
+        blocktype = IRON_BLOCK
+        value = 2
+        answer = 2
+    }
+    //% blockId=classification2_1 block="🏛 : 공식 자료"
+    export function data2_1(): void {
+        blocktype = IRON_BLOCK
+        value = 1
+        answer = 2
+    }
+    //% blockId=classification2 block="2번 문제"
+    export function Q2(): void {
+    }
+
+    //% blockId=classification1_5 block="📢 : 광고/출처 모름"
+    export function data1_5(): void {
+        blocktype = GOLD_BLOCK
+        value = 5
+        answer = 1
+    }
+    //% blockId=classification1_4 block="💬 : 개인 의견"
+    export function data1_4(): void {
+        blocktype = GOLD_BLOCK
+        value = 4
+        answer = 1
+    }
+    //% blockId=classification1_3 block="📰 : 뉴스/책 자료"
+    export function data1_3(): void {
+        blocktype = GOLD_BLOCK
+        value = 3
+        answer = 1
+    }
+    //% blockId=classification1_2 block="👩‍🏫 : 전문가 자료"
+    export function data1_2(): void {
+        blocktype = GOLD_BLOCK
+        value = 2
+        answer = 1
+    }
+    //% blockId=classification1_1 block="🏛 : 공식 자료"
+    export function data1_1(): void {
+        blocktype = GOLD_BLOCK
+        value = 1
+        answer = 1
+    }
+    //% blockId=classification1 block="1번 문제"
+    export function Q1(): void {
+    }
+
+    //% blockId=classification_answer block="이 정보의 종류는?"
+    export function classification(handler: () => void): void {
+        let delay: number = 500
+
+        handler()
+        loops.forever(function () {
+            loops.pause(delay)
+            classification_test()
+        })
+    }
+
+}
+
+enum PlayerNumber {
+    //% block="1번"
+    One = 1,
+    //% block="2번"
+    Two = 2,
+    //% block="3번"
+    Three = 3,
+    //% block="4번"
+    Four = 4,
+    //% block="5번"
+    Five = 5,
+    //% block="6번"
+    Six = 6,
+    //% block="7번"
+    Seven = 7,
+    //% block="8번"
+    Eight = 8,
+    //% block="9번"
+    Nine = 9,
+    //% block="10번"
+    Ten = 10,
+    //% block="11번"
+    Eleven = 11,
+    //% block="12번"
+    Twelve = 12,
+    //% block="13번"
+    Thirteenth = 13,
+    //% block="14번"
+    Fourteen = 14,
+    //% block="15번"
+    Fifteen = 15,
+}
+
+enum RoomNumber {
+    //% block="1번"
+    One = 1,
+    //% block="2번"
+    Two = 2,
+    //% block="3번"
+    Three = 3,
+    //% block="4번"
+    Four = 4,
+    //% block="5번"
+    Five = 5,
+    //% block="6번"
+    Six = 6,
+    //% block="7번"
+    Seven = 7,
+    //% block="8번"
+    Eight = 8,
+}
+
+enum Roomtype {
+    //% block="수업"
+    Class = 1,
+    //% block="시험"
+    Examination = 2
+}
+
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#455A64 icon="\uf0ad" block="교사용 컨트롤 시스탬"
+namespace mapadmin {
+    let class_number = ""
+    //%blockId=task block="어떤 작업을 시행 하실건가요? "
+    export function task(handler: () => void): void {
+        player.execute("tag " + "@s" + "[tag=!map_admin] "+"add "+"map_admin")
+        handler()
+    }
+    /**
+    * 맵의 동작을 초기화 합니다.
+    */
+    //%blockId=initialization block="초기화"
+    export function initialization(): void {
+        player.execute("function admin/I")
+    }
+    /**
+    * 맵의 모든 것들을 초기화 합니다.
+    * (맵에 심각한 문제가 발생시에만 시행)
+    */
+    //%blockId=factory_initialization block="공장 초기화"
+    export function factory_initialization(): void {
+        player.execute("function admin/FI")
+    }
+    /**
+     * 특정방의 문을 개방합니다.
+     */
+    //%handlerStatement=0
+    //%blockId=opendoor_class block="$n째 날 $r방 개방"
+    export function opendoor_class(n: RoomNumber, r: Roomtype): void {
+        if ((!((n == 1 && r == 2) || (n == 6 && r == 1) || n == 7 || n == 8))){
+            let s = "day" + n + (r == Roomtype.Class ? "_class" : "_examination")
+            player.execute("tag " + "@s" + "[tag=map_admin] " + "add " + s)
+            player.execute("tag " + "@s" + "[tag=map_admin] " + "add " + "door_open")
+        }else{
+            player.say('§e이런! 해당 방은 존재하지 않습니다!')
+        }
+    }
+    /**
+     * 특정방의 수업을 작동시킵니다.
+     */
+    //%blockId=start_class block="$n째 날 수업 진행"
+    export function start_class(n: RoomNumber): void {
+        if(n != 1 && n != 7 && n != 8){
+            player.execute("function environment/enter_examinations/"+n)
+        }else{
+            player.say('§e이런! 해당 날의 수업은 작동시킬 것이 없습니다!')
+        }
+    }
+    /**
+     * 특정 학생의 수업을 종료시킵니다.
+     */
+    //%blockId=end_class block="$p학생의 수업 종료"
+    export function end_class(p: PlayerNumber): void {
+        player.execute("execute " + "@a" + "[tag=examination_" + p + "] ~ ~ ~ " +"function environment/exit_examination")
+    }
+    /**
+     * 특정날의 건축 수업을 시작합니다.
+     */
+    //%blockId=start_build_class block="$p째 날의 건축 수업으로 이동"
+    export function start_build_class(n: RoomNumber): void {
+        if(n != 1 && n != 6){
+            player.execute("function environment/start_build_class/"+n)
+        }else{
+            player.say('§e이런! 해당 날의 건축 수업은 없습니다!')
+        }
+    }
+    /**
+     * 특정날의 건축 수업을 끝냅니다.
+     */
+    //%blockId=end_build_class block="건축 수업 종료해서 시작지점으로 이동"
+    export function end_build_class(): void {
+        player.execute("function environment/end_build_class/def")
+    }
+}
